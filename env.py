1ed828df2ae1ea32c72bc354edd25e3641812b66
Anjana1141 <anjanaicse@gmail.com>
2024-05-08 17:41:39 +0530

    changed commit

---
+++ env.py
@@ -0,0 +1,13 @@
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+DATA_COLLECTION_BUCKET = 'testoohybucket'
+AWS_ACCESS_KEY_ID = 'AKIATCKARCOUJYVJ3T4U'
+AWS_STORAGE_BUCKET_NAME = 'testoohybucket'
+AWS_SECRET_ACCESS_KEY= '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'
+AWS_S3_REGION_NAME =  'ap-south-1'
+AWS_S3_SIGNATURE_NAME = 's3v4'
+HOST = 'mongodb://54.166.103.115:27017/'
+NAME= 'dataentries'
